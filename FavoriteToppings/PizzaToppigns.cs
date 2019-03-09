98edcd175db8a9f83136cd14a8285da9fbdc0a48
WaysideTester <dylanmurray211@outlook.com>
2019-03-09 15:54:31 -0600

    correctly identifying distinct topping combos

---
+++ FavoriteToppings/PizzaToppigns.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace FavoriteToppings
+{
+    class PizzaToppigns
+    {
+        public List<string> Toppings;
+    }
+}
--- FavoriteToppings/Program.cs
+++ FavoriteToppings/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
 
 namespace FavoriteToppings
 {
@@ -6,6 +10,22 @@ namespace FavoriteToppings
     {
         static void Main(string[] args)
         {
+            var JsonPizzaOrders = File.ReadAllText(@"D:\Coding\backend\Orientation\Exersizes\FavoriteToppings\FavoriteToppings\NewFolder\pizzas.json");
+            var PizzaOrders = JsonConvert.DeserializeObject<List<PizzaToppigns>>(JsonPizzaOrders);
+
+            List<string> StringOrders = new List<string>();
+
+            foreach (var order in PizzaOrders)
+            {
+                StringOrders.Add(string.Join(",", order.Toppings));
+            }
+            IEnumerable<string> DistinctToppings = StringOrders.Distinct();
+
+            foreach (var combo in DistinctToppings)
+            {
+                Console.WriteLine(combo);
+            }
+
             Console.WriteLine("Hello World!");
         }
     }
