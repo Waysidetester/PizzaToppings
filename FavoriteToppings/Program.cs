1948d2fdee1cc593dd1d3e7a582f1102304a7991
WaysideTester <dylanmurray211@outlook.com>
2019-03-09 16:02:33 -0600

    comments for clarity

--- FavoriteToppings/Program.cs
+++ FavoriteToppings/Program.cs
@@ -13,20 +13,23 @@ namespace FavoriteToppings
             var JsonPizzaOrders = File.ReadAllText(@"D:\Coding\backend\Orientation\Exersizes\FavoriteToppings\FavoriteToppings\NewFolder\pizzas.json");
             var PizzaOrders = JsonConvert.DeserializeObject<List<PizzaToppigns>>(JsonPizzaOrders);
 
+            // Creating an empty list of strings to access later
             List<string> StringOrders = new List<string>();
 
+            /* Looping over JSON PizzaOrders to join at "," and give ability to
+             * use the "Distinct()" IEnumerable method on. */
             foreach (var order in PizzaOrders)
             {
+                // Joins Toppings array items together and adds to the StringOrders
                 StringOrders.Add(string.Join(",", order.Toppings));
             }
+
+            /* Creates a IEnumerable variable to run the Distinct method on.
+             * I can now see which combos are distinct from other combos */
             IEnumerable<string> DistinctToppings = StringOrders.Distinct();
 
-            foreach (var combo in DistinctToppings)
-            {
-                Console.WriteLine(combo);
-            }
-
             Console.WriteLine("Hello World!");
+            Console.ReadKey();
         }
     }
 }
